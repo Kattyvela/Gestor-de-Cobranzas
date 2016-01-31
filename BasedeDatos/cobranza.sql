b9842c8308807623054ca60fee530dc6980b01ed
David Tayupanta <davidotb91@hotmail.com>
2016-01-30 23:11:13 -0500

    correccion de la base de datos

--- BasedeDatos/cobranza.sql
+++ BasedeDatos/cobranza.sql
@@ -3,12 +3,12 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mydb` ;
+USE `gestorcobranza` ;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Usuario_unidad_negocio`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Usuario_unidad_negocio` (
+CREATE  TABLE IF NOT EXISTS `Usuario_unidad_negocio` (
   `id_Usuario_unidad_negocio` INT NOT NULL AUTO_INCREMENT ,
   `id_usuario` INT NOT NULL ,
   `id_unidad_negocio` INT NULL ,
@@ -20,7 +20,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Cliente`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Cliente` (
+CREATE  TABLE IF NOT EXISTS `Cliente` (
   `id_Cliente` INT NOT NULL AUTO_INCREMENT ,
   `cedula_ruc` VARCHAR(13) NOT NULL ,
   `cod_cliente` VARCHAR(8) NOT NULL ,
@@ -35,7 +35,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Mora`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Mora` (
+CREATE  TABLE IF NOT EXISTS `Mora` (
   `id_Mora` INT NOT NULL AUTO_INCREMENT ,
   `descripcion` VARCHAR(150) NOT NULL ,
   `nro_factura` INT NOT NULL ,
@@ -50,12 +50,12 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Mora` (
   INDEX `fk_Mora_Cliente1` (`Cliente_id_Cliente` ASC) ,
   CONSTRAINT `fk_Mora_Usuario_unidad_negocio1`
     FOREIGN KEY (`Usuario_unidad_negocio_id_Usuario_unidad_negocio` )
-    REFERENCES `mydb`.`Usuario_unidad_negocio` (`id_Usuario_unidad_negocio` )
+    REFERENCES `Usuario_unidad_negocio` (`id_Usuario_unidad_negocio` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Mora_Cliente1`
     FOREIGN KEY (`Cliente_id_Cliente` )
-    REFERENCES `mydb`.`Cliente` (`id_Cliente` )
+    REFERENCES `Cliente` (`id_Cliente` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -64,7 +64,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Tipo_Gestion`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Tipo_Gestion` (
+CREATE  TABLE IF NOT EXISTS `Tipo_Gestion` (
   `id_Tipo_Gestion` INT NOT NULL AUTO_INCREMENT ,
   `nombre` VARCHAR(45) NOT NULL ,
   `registro_activo` TINYINT(1) NOT NULL ,
@@ -75,7 +75,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Resultado`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Resultado` (
+CREATE  TABLE IF NOT EXISTS `Resultado` (
   `id_Resultado` INT NOT NULL AUTO_INCREMENT ,
   `nombre` VARCHAR(45) NOT NULL ,
   `registro_activo` TINYINT(1) NOT NULL ,
@@ -86,7 +86,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Gestion`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Gestion` (
+CREATE  TABLE IF NOT EXISTS `Gestion` (
   `id_Gestion` INT NOT NULL AUTO_INCREMENT ,
   `fecha_gestion` DATE NOT NULL ,
   `observaciones` VARCHAR(80) NOT NULL ,
@@ -99,17 +99,17 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Gestion` (
   INDEX `fk_Gestion_Resultado1` (`Resultado_id_Resultado` ASC) ,
   CONSTRAINT `fk_Gestion_Tipo_Gestion1`
     FOREIGN KEY (`Tipo_Gestion_id_Tipo_Gestion` )
-    REFERENCES `mydb`.`Tipo_Gestion` (`id_Tipo_Gestion` )
+    REFERENCES `Tipo_Gestion` (`id_Tipo_Gestion` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Gestion_Mora1`
     FOREIGN KEY (`Mora_id_Mora` )
-    REFERENCES `mydb`.`Mora` (`id_Mora` )
+    REFERENCES `Mora` (`id_Mora` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Gestion_Resultado1`
     FOREIGN KEY (`Resultado_id_Resultado` )
-    REFERENCES `mydb`.`Resultado` (`id_Resultado` )
+    REFERENCES `Resultado` (`id_Resultado` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -118,7 +118,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Respuesta`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Respuesta` (
+CREATE  TABLE IF NOT EXISTS `Respuesta` (
   `id_Respuesta` INT NOT NULL AUTO_INCREMENT ,
   `respuesta_cliente` VARCHAR(45) NULL ,
   `fecha_compromiso_pago` DATE NULL ,
@@ -128,7 +128,7 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Respuesta` (
   INDEX `fk_Respuesta_Gestion_idx` (`Gestion_id_Gestion` ASC) ,
   CONSTRAINT `fk_Respuesta_Gestion`
     FOREIGN KEY (`Gestion_id_Gestion` )
-    REFERENCES `mydb`.`Gestion` (`id_Gestion` )
+    REFERENCES `Gestion` (`id_Gestion` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -137,7 +137,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Forma_Pago`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Forma_Pago` (
+CREATE  TABLE IF NOT EXISTS `Forma_Pago` (
   `id_Forma_Pago` INT NOT NULL AUTO_INCREMENT ,
   `nombre` VARCHAR(45) NOT NULL ,
   `registro_activo` TINYINT(1) NOT NULL ,
@@ -149,7 +149,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Abono_Deuda`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Abono_Deuda` (
+CREATE  TABLE IF NOT EXISTS `Abono_Deuda` (
   `id_Abono_Deuda` INT NOT NULL AUTO_INCREMENT ,
   `fecha_abono` DATE NOT NULL ,
   `abono` DOUBLE NOT NULL ,
@@ -162,12 +162,12 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Abono_Deuda` (
   INDEX `fk_Abono_Deuda_Forma_Pago1` (`Forma_Pago_id_Forma_Pago` ASC) ,
   CONSTRAINT `fk_Abono_Deuda_Mora1`
     FOREIGN KEY (`Mora_id_Mora` , `Mora_Usuario_unidad_negocio_id_Usuario_unidad_negocio` , `Mora_Cliente_id_Cliente` )
-    REFERENCES `mydb`.`Mora` (`id_Mora` , `Usuario_unidad_negocio_id_Usuario_unidad_negocio` , `Cliente_id_Cliente` )
+    REFERENCES `Mora` (`id_Mora` , `Usuario_unidad_negocio_id_Usuario_unidad_negocio` , `Cliente_id_Cliente` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Abono_Deuda_Forma_Pago1`
     FOREIGN KEY (`Forma_Pago_id_Forma_Pago` )
-    REFERENCES `mydb`.`Forma_Pago` (`id_Forma_Pago` )
+    REFERENCES `Forma_Pago` (`id_Forma_Pago` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
